4226bdef4fca9d4ee38ea03ed0635a950fa2f09c
Shimi <shimi@totango.com>
2015-07-13 23:09:39 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+var net = require('net');
+
+module.exports = Graphite;
+
+function Graphite(host, port, charSet, timeout, timeoutListener) {
+  this.host = host;
+  this.port = port;
+  this.charSet = charSet;
+  this.timeout = timeout;
+  this.timeoutListener = timeoutListener;
+  this.eventsCallbacks = [];
+}
+
+Graphite.prototype.connect = function(connectListener) {
+  this.socket = net.connect({host: this.host, port: this.port}, connectListener);
+  this.socket.setEncoding(this.charSet);
+  
+  var self = this;
+  this.socket.setTimeout(this.timeout, function(){
+
+    self.timeoutListener();
+    self.end();
+    self.connect(connectListener);
+  });
+
+  this.eventsCallbacks.forEach(function (eventCallback) {
+    this.socket.on(eventCallback.event, eventCallback.callback);
+  }, this);
+}
+
+Graphite.prototype.on = function(event, callback) {
+  var eventCallback = {};
+  eventCallback.event = event;
+  eventCallback.callback = callback;
+  this.eventsCallbacks.push(eventCallback);
+
+  if (this.socket) {
+    this.socket.on(event, callback);
+  }
+}
+
+Graphite.prototype.end = function() {
+  if (this.socket) {
+    this.socket.end();
+  }
+};
+
+Graphite.prototype.write = function(metrics, timestamp, callback) {
+  timestamp = timestamp || Date.now();
+  timestamp = Math.floor(timestamp / 1000);
+
+  var flatt = this.flatten(metrics);
+  var lines = '';
+  for (var key in flatt) {
+    var value = flatt[key];
+    lines += [key, value, timestamp].join(' ') + '\n';
+  }
+
+  try {
+    this.socket.write(lines);
+  } catch (err) {
+    if (callback) {
+      callback(err);
+    } else {
+      throw err;
+    }
+  }
+};
+
+Graphite.prototype.flatten = function(obj, flat, prefix) {
+  flat   = flat || {};
+  prefix = prefix || '';
+
+  for (var key in obj) {
+    var value = obj[key];
+    if (typeof value === 'object') {
+      this.flatten(value, flat, prefix + key + '.');
+    } else {
+      flat[prefix + key] = value;
+    }
+  }
+
+  return flat;
+};
---
+++ test/graphite-test.js
@@ -0,0 +1,124 @@
+var assert = require('assert');
+
+var sinon = require('sinon')
+
+var net = require("net")
+var Mitm = require("mitm")
+var mitm = Mitm()
+
+var Graphite = require('../index');
+
+describe('Graphite', function () {
+  describe('write', function () {
+
+    var metrics = {
+      'pre1' : {
+        'pre2' : {
+          'key1' : 'value1',
+          'key2' : 'value2',
+        },
+      },
+      'key3' : 'value3'};
+  
+    var metricsLines = 'pre1.pre2.key1 value1 1234567890\npre1.pre2.key2 value2 1234567890\nkey3 value3 1234567890\n';
+
+    it('should send object as lines to server', function (done) {
+
+      var mock = {};
+      mock.write = function(data) {
+        assert.equal(data, metricsLines, 'object to lines mapping is broken');
+        done();
+      }
+
+      var graphite = new Graphite();
+      graphite.socket = mock;
+  
+      graphite.write(metrics, 1234567890000);
+    });
+
+    it('should add time if not provided', function (done) {
+
+      var mock = {};
+      var metricsLinesWithTimeZero = 'pre1.pre2.key1 value1 0\npre1.pre2.key2 value2 0\nkey3 value3 0\n';
+      mock.write = function(data) {
+        var pattern = new RegExp('1234567890', "g");
+        assert.equal(data, metricsLines.replace(pattern, '0'), 'object to lines mapping is broken');
+        clock.restore();
+        done();
+      }
+
+      var graphite = new Graphite();
+      graphite.socket = mock;
+  
+      var clock = sinon.useFakeTimers();
+      graphite.write(metrics);
+    });
+
+    it('should throw error if callback was not provided', function () {
+  
+      var graphite = new Graphite();
+
+      assert.throws(
+        function() {
+          graphite.write(metrics);
+        },
+        Error
+      );
+    });
+
+    it('should not throw error if callback was provideds', function () {
+  
+      var graphite = new Graphite();
+
+      assert.doesNotThrow(
+        function() {
+          graphite.write(metrics, 1, function() {});
+        }
+      );
+    });
+
+    it('should call the timeout callback on timeout', function (done) {
+  
+      mitm.on("connection", function(socket) { socket.write('connect')  })
+
+      var done_called = false;
+
+      var graphite = new Graphite('someHostAddress', 2003, 'UTF-8', 1, function() {
+        graphite.end();
+        if (!done_called) {
+          done();
+          done_called = true;
+        }
+      });
+      graphite.connect(null);
+    })
+  })
+
+  describe('connect', function () {
+    it('should connect to the server and call the callback method', function (done) {
+      mitm.on("connection", function(socket) { socket.write('connect')  })
+    
+      var graphite = new Graphite('someHostAddress', 2003, 'UTF-8', 1, null);
+      graphite.connect(function() {
+        graphite.end();
+        done();
+      });
+    });
+  })
+
+  describe('on(error)', function () {
+    it('should call the error callback', function (done) {
+      var graphite = new Graphite('someHostAddress', 2003, 'UTF-8', 1000000);
+
+      graphite.on('error', function() {
+        graphite.end();
+        done();
+      });
+      graphite.connect(null);
+      graphite.end();
+      graphite.write('sdfsd');
+    
+    });
+  });
+
+})
